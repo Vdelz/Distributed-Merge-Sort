20fc28e50a95aa94ba93264c0cde0465ab9b182c
Vincenzo <91189062+Vdelz@users.noreply.github.com>
2025-01-11 01:11:15 +0100

    Update createFile.py

--- createFile.py
+++ createFile.py
@@ -1,14 +1,19 @@
-from random import randint
-N=1000
-f=open("largefile","w")
-k=0
-while True:
- n=randint(0,N)
- f.write(str(n)+"\n")
- k=k+1
- if k==100:
-  f.close()
-  break
+"""
+https://en.wikipedia.org/wiki/External_sorting
+https://en.wikipedia.org/wiki/K-way_merge_algorithm
 
+"""
+import numpy as np
 
+L=2
 
+for i in range(8**L):
+    f=open("chunk"+str(i).zfill(L)+".csv","w")
+    k=0
+    while True:
+        n = np.random.randn(3)
+        ok = f.write(";".join([str(j) for j in n])+"\n")
+        k=k+1
+        if k==64**L:
+            f.close()
+            break
